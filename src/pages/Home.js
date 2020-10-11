e3f7663b3e0763b6520909e72ae1dfb8d207c40a
sulada <hibino.aoom@gmail.com>
2020-10-11 08:09:21 -0700

    deploy app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
-import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
 import view from "../image/view1.jpg";
-// import "./Home.css";
 
 function About() {
   return (
@@ -13,15 +9,6 @@ function About() {
         <h1>SULADA SUDJAI</h1>
         <h2>Portfolio</h2>
       </Hero>
-      {/* <Container style={{ marginTop: 30 }}>
-        <Row>
-          <Col size="md-12">
-            <div className="welcome">
-            <p>Welcome To Sulada Portfolio!</p>
-            </div>
-          </Col>
-        </Row>
-        </Container> */}
     </div>
   );
 }
